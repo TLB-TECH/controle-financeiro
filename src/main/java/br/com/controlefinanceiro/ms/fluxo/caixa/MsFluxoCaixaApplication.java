dbe0120c17f236cce1e7e3b2ea73eb376c27280b
Taciano <taciolb@gmail.com>
2026-06-19 19:09:51 -0300

    feat: inicializa projeto ms-fluxo-caixa

---
+++ src/main/java/br/com/controlefinanceiro/ms/fluxo/caixa/MsFluxoCaixaApplication.java
@@ -0,0 +1,13 @@
+package br.com.controlefinanceiro.ms.fluxo.caixa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFluxoCaixaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFluxoCaixaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/controlefinanceiro/ms/fluxo/caixa/MsFluxoCaixaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.controlefinanceiro.ms.fluxo.caixa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFluxoCaixaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
